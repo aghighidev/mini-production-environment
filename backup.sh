e4030ceff1955a0e7fbeb21ea7db8c76c76622e9
aghighidev <aghighi.dev@gmail.com>
2026-09-15 22:09:47 +0330

    new featuers

--- backup.sh
+++ backup.sh
@@ -2,4 +2,10 @@
 
 read -s -p $'\e[32menter your root pass :\e[0m' rootpass
 echo ""
-docker compose exec -T database mysqldump -u root -p$rootpass database > backup.sql
+date=$(date +"%Y-%m-%d_%H-%M-%S")
+docker compose exec -T database mysqldump -u root -p"$rootpass" database > backups/backup_$date.sql
+if [[ $? -eq 0 ]]; then
+	echo -e "\e[32mbackup succesfull : backups/backup_$date.sql"
+else
+	echo -e "\e[31mbackup failed :(\e[0m"
+fi
